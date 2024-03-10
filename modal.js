9d4ac5eaabee789bcb224f5a6f93a4fe46e16bfd
dryastr <darayastore01@gmail.com>
2024-03-10 16:30:33 +0700

    add esc for close modal

--- modal.js
+++ modal.js
@@ -10,12 +10,24 @@ document.querySelectorAll("[id^='openModal']").forEach(function(el) {
     });
 });
 
-document.querySelectorAll(".modal-background").forEach(function(el) {
+document.querySelectorAll(".modal-background, .modal-close").forEach(function(el) {
     el.addEventListener("click", function(event) {
-        if (event.target === this) {
+        if (event.target === this || event.target.closest(".modal-close")) { // Memeriksa apakah tombol close atau elemen terdekat dari tombol close yang ditekan
             var modalId = this.parentElement.getAttribute("id");
             document.getElementById(modalId).classList.remove("two");
             document.body.classList.remove("modal-active");
         }
     });
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") { // Memeriksa apakah tombol yang ditekan adalah "Escape"
+        var openModals = document.querySelectorAll(".modal-container.two"); // Mengambil semua modal yang terbuka
+        if (openModals.length > 0) { // Memeriksa apakah ada modal yang terbuka
+            openModals.forEach(function(modal) {
+                modal.classList.remove("two");
+            });
+            document.body.classList.remove("modal-active");
+        }
+    }
 });
